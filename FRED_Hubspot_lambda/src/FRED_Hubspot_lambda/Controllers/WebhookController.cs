441388e8d74d65ca2a57a3c673621878d23b9c84
hrTomal <hr.tomal@gmail.com>
2024-11-29 03:29:06 +0600

    init

---
+++ FRED_Hubspot_lambda/src/FRED_Hubspot_lambda/Controllers/WebhookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FRED_Hubspot_lambda.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WebhookController : ControllerBase
+{
+    
+    [HttpPost]
+    [Route("receive")]
+    public IActionResult ReceiveWebhook([FromBody] dynamic webhookData)
+    {
+        try
+        {
+            Console.WriteLine(webhookData);
+            return Ok(new { message = "Webhook received successfully", data = webhookData });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error processing webhook: " + ex.Message);
+            return StatusCode(500, new { message = "Internal server error", error = ex.Message });
+        }
+    }
+
+}
---
+++ FRED_Hubspot_lambda/src/FRED_Hubspot_lambda/Functions/Function.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+
+namespace FRED_Hubspot_lambda.Functions
+{
+    public class Function : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<Startup>();  // Specify your Startup class for the app configuration
+        }
+    }
+}
